7840ed37f74f27b90f2ab01ab2167b8d6fa1f721
Casandra <casandragrullon@pursuit.org>
2019-10-29 17:02:42 -0400

    "started project
    "

---
+++ ColorGame/ColorGame/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ColorGame
+//
+//  Created by casandra grullon on 10/29/19.
+//  Copyright © 2019 casandra grullon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var colorInPlay: UIImageView!
+    
+    @IBOutlet weak var gamePrompt: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    @IBAction func rgbButtons(_ button: UIButton) {
+        
+        
+    }
+    
+    
+    
+}
+
